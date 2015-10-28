836ce797d7d26e8ac68b296a75845cd1415c95f2
P. Douglas Reeder <reeder.29@gmail.com>
2015-10-28 00:54:18 -0400

    Toasts are positioned relative to their parent, not the window.

--- samples/toasters.js
+++ samples/toasters.js
@@ -5,22 +5,36 @@ enyo.kind({
     name: "animated.sample.Toasters",
     kind: "FittableRows",
     fit: true,
-    components:[
-        {kind: "animated.ToastBottomSlide", style: 'width: 12em', components: [
-            {content: "I'm a ToastBottomSlide!"}
-        ]},
-        {kind: "animated.ToastBottomLeftCurve", style: 'width: 13em', components: [
-            {name: 'question', content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", style: 'margin-bottom: 10px'},
-            {name: 'toasterYesBtn', kind: 'onyx.Button', content: $L('Yes'), ontap: 'toggleToastBottomLeftCurve'},
-            {kind: 'onyx.Button', content: $L('No'), ontap: 'toggleToastBottomLeftCurve', style: 'float: right;'}
-        ]},
-        {kind: "onyx.Toolbar", components: [
-            {kind: "onyx.Button", content: "ToastBottomSlide", ontap: "toggleToastBottomSlide"},
-            {kind: "onyx.Button", content: "ToastBottomLeftCurve", ontap: "toggleToastBottomLeftCurve"}
-        ]},
-        {kind: "enyo.Scroller", fit: true, components: [
-            {name: "main", classes: "nice-padding", allowHtml: true}
-        ]},
+    components: [
+        {
+            name: 'mainPane',
+            kind: "FittableRows",
+            fit: true,
+            // required to keep toasts under secondaryPane
+            style: 'transform-style: preserve-3d',
+            components:[
+                {kind: "animated.ToastBottomSlide", style: 'width: 12em', components: [
+                    {content: "I'm a ToastBottomSlide!"}
+                ]},
+                {kind: "animated.ToastBottomLeftCurve", style: 'width: 13em', components: [
+                    {name: 'question', content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", style: 'margin-bottom: 10px'},
+                    {name: 'toasterYesBtn', kind: 'onyx.Button', content: $L('Yes'), ontap: 'toggleToastBottomLeftCurve'},
+                    {kind: 'onyx.Button', content: $L('No'), ontap: 'toggleToastBottomLeftCurve', style: 'float: right;'}
+                ]},
+                {kind: "onyx.Toolbar", components: [
+                    {kind: "onyx.Button", content: "ToastBottomSlide", ontap: "toggleToastBottomSlide"},
+                    {kind: "onyx.Button", content: "ToastBottomLeftCurve", ontap: "toggleToastBottomLeftCurve"}
+                ]},
+                {kind: "enyo.Scroller", fit: true, components: [
+                    {name: "main", classes: "nice-padding", allowHtml: true}
+                ]},
+            ]
+        },
+        {
+            name: 'secondaryPane',
+            content: "I'm a pane above the toasters, which complicates the logical model.",
+            style: 'height: 4em; transform: translateZ(2px); background-color: #eee; padding: 1em'
+        }
     ],
     toggleToastBottomSlide: function(inSender, inEvent) {
         this.$.toastBottomSlide.set('showing', ! this.$.toastBottomSlide.get('showing'));
